af087001283e13cebe791516c4502bd156c696ce
Aiswarya-ally <aiswaryaprakasan2000@gmail.com>
2023-10-27 12:09:04 +0530

    Moved DistributerController into eLPG

---
+++ src/main/java/com/eLPG/controller/DistributerController.java
@@ -0,0 +1,67 @@
+package dis.eLPG.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import dis.eLPG.entity.IndentDetails;
+
+import dis.eLPG.service.IndentService;
+
+
+
+@Controller
+public class DistributerController 
+{
+	@Autowired
+	IndentService indservice;
+	
+	@GetMapping("/")
+	public String Consumer()
+	{
+		return "index";
+	}
+	
+
+	@PostMapping("/saveindent")
+	public String saveMethod(@ModelAttribute("ind") IndentDetails log)
+	{
+		indservice.save(log);
+		 return "redirect:/stattrack";
+		
+	}
+	
+
+	
+	@GetMapping("/cylreq")
+	public String custRequest()
+	{
+		return "cylinderrequest";
+	}
+	
+	@GetMapping("/custregister")
+	public String custRegister()
+	{
+		return "customerregistration";
+	}
+	
+	@GetMapping("/stattrack")
+	public String statusTracking()
+	{
+		return "statustrack";
+	}
+
+	
+	@GetMapping("/indent")
+	public String indentRequest()
+	{
+		return "indentform";
+		
+	}
+	
+}
