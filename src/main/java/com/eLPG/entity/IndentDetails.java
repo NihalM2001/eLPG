5998bbe94a9b83e978286b119165074868f8bc4c
Aiswarya-ally <aiswaryaprakasan2000@gmail.com>
2023-10-27 12:13:44 +0530

    Moved IndentDetails and LoginDetails into eLPG

---
+++ src/main/java/com/eLPG/entity/IndentDetails.java
@@ -0,0 +1,44 @@
+package dis.eLPG.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class IndentDetails 
+{
+	@Id
+	@GeneratedValue
+	private int distributerid;
+	private String username;
+	private int count;
+	public int getDistributerid() {
+		return distributerid;
+	}
+	public void setDistributerid(int distributerid) {
+		this.distributerid = distributerid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/eLPG/entity/LoginDetails.java
@@ -0,0 +1,43 @@
+package dis.eLPG.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LoginDetails 
+{
+	@Id
+	@GeneratedValue
+	private int userid;
+	private String username,password;
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
